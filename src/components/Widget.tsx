b5b61d23502e6427d1bae36d4d544dccb85c1aaf
Fabricio Borgobello <fborgobello@gmail.com>
2024-11-16 10:28:07 +0100

    motion

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,21 @@
+import { LazyMotion, domAnimation } from "motion/react";
+import * as m from "framer-motion/m";
+
 export const Widget = () => {
   return (
-    <div
-      style={{ border: "1px solid #ccc", padding: "10px", maxWidth: "300px" }}
-    >
-      <h3>My Widget</h3>
-      <p>This is a React widget embedded in another site!</p>
-    </div>
+    <LazyMotion features={domAnimation}>
+      <m.div animate={{ opacity: 1 }}>
+        <div
+          style={{
+            border: "1px solid #ccc",
+            padding: "10px",
+            maxWidth: "300px",
+          }}
+        >
+          <h3>My Widget</h3>
+          <p>This is a React widget embedded in another site!</p>
+        </div>
+      </m.div>
+    </LazyMotion>
   );
 };
